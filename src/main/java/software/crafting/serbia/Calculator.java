3bb6d3f9629cef3827086a405458743fe9434ef5
Filip Petrovic <fpetrovic92@gmail.com>
2019-09-04 19:41:58 +0200

    Extract constaint

--- src/main/java/software/crafting/serbia/Calculator.java
+++ src/main/java/software/crafting/serbia/Calculator.java
@@ -2,11 +2,13 @@ package software.crafting.serbia;
 
 public class Calculator {
 
+  private static final double UTAH_TAX_RATE = 1.0685;
+
   public String calculate(String amountString, String priceString) {
     final int amount = Integer.parseInt(amountString);
     final double price = Double.parseDouble(priceString);
 
-    final double result = amount * price * 1.0685;
+    final double result = amount * price * UTAH_TAX_RATE;
 
     return String.format("$%.2f", result);
   }
